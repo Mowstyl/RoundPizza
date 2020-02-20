6981b31b16c2188668de3a986863231f6128916e
Hernán Indíbil de la Cruz Calvo <indikalesken@hotmail.com>
2020-02-20 16:48:40 +0100

    Photo problem

--- lib/compile.bat
+++ lib/compile.bat
@@ -1,6 +1,6 @@
 cls
 del pizza.o
 del pizza.dll
-gcc -c -fopenmp -std=c99 -Wall -Wextra -m64 -fpic -o pizza.o pizza.c
+gcc -c -fopenmp -std=c99 -Wall -Wextra -Werror -pedantic-errors -m64 -fpic -o pizza.o pizza.c
 gcc -shared -o pizza.dll pizza.o -fopenmp -std=c99 -Wall -Wextra -Werror -pedantic-errors -m64 -lm
 pause
